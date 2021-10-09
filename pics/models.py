42908bff5b0d15a76222a88aef54d9d2074bf452
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-10-09 12:37:12 +0300

    add location model

--- pics/models.py
+++ pics/models.py
@@ -13,3 +13,12 @@ class Category(models.Model):
         
     class Meta:
         ordering=['category']
+
+class Location(models.Model):
+    location= models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.location
+    
+    def save_location(self):
+        self.save()
