3fc2d6635fae7247a088ad1bc793dacc38f26856
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-10-09 11:49:23 +0300

    adding urls

--- pics/views.py
+++ pics/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http  import HttpResponse
 
 # Create your views here.
-def gallery(request):
-    return render(request, 'welcome.html')
+def home(request):
+    return render(request, 'home.html')
