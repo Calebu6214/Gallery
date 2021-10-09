8728779dd594d0f5a0a69a32f28ae19a5d6c29c1
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-10-09 12:49:05 +0300

    add loaction view method

--- pics/views.py
+++ pics/views.py
@@ -8,3 +8,12 @@ def home(request):
     locations=Location.get_locations()
 
     return render(request, 'all-gallery/home.html',{"images":images,"locations":locations})
+
+def all_locations(request):
+    locations=Location.get_locations()
+    return render(request,'all-gallery/home.html',{"locations":locations})
+
+def image_by_location(request,location):
+    locations=Location.get_locations()
+    images=Image.get_image_by_location(location)
+    return render(request,'all-gallery/location.html',{"images":images,"location":location,"locations":locations})    
