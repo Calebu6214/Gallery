532f52d1899f948277439afaedfd950d20b060a2
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-10-09 11:51:45 +0300

    add category class

--- pics/models.py
+++ pics/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+# from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Category(models.Model):
+    category = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.category
+    
+    def save_category(self):
+        self.save()
+        
+    class Meta:
+        ordering=['category']
